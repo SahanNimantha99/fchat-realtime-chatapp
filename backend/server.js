ad952e58a78ab86aa1637553d6860853dcb12063
Sahan Nimantha <e19205@eng.pdn.ac.lk>
2024-05-01 15:29:42 +0530

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
-import express from "express"; //importing express
-import dotenv from "dotenv"; //importing dotenv
-import authRoutes from "./routes/auth.routes.js"; //importing the authRoutes
-const app = express(); //creating an express app
+import express from "express"; // Importing express
+import dotenv from "dotenv"; // Importing dotenv
 
-dotenv.config(); //configuring the dotenv
+import authRoutes from "./routes/auth.routes.js"; // Importing the authRoutes
+import connectToMongoDB from "./db/connectToMongoDB.js"; // Importing the connectToMongoDB function
 
-const PORT = process.env.PORT || 5000; //port number
+const app = express(); // Creating an express app
+const PORT = process.env.PORT || 5000; // Port number
 
-app.get("/", (req, res) => {
-  //root route http://localhost:5000/
-  res.send("API is running......");
+dotenv.config(); // Configuring the dotenv
+
+app.use(express.json()); // Middleware to parse JSON
+
+// Routes
+app.use("/api/auth", authRoutes); // Using the authRoutes
+
+// Root route
+// app.get("/", (req, res) => {
+//   //http://localhost:5000/
+//   res.send("API is running...");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB(); // Connecting to MongoDB
+  console.log(`Server is running on port ${PORT}`);
 });
-
-app.use("/api/auth", authRoutes); //using the authRoutes
-
-app.listen(PORT, () => console.log("Server is running on port", PORT));
